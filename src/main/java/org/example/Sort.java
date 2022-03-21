a0a0f7c80898fd244a95cbb05c5ff7096ae1b7ee
ajames <ajames@vashonsd.org>
2022-03-21 10:24:55 -0700

    Initial commit, with test for merge and sortTwo methods.

---
+++ src/main/java/org/example/Sort.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Sort {
+
+    public static void sortTwo(int[] nums) {
+
+    }
+
+    public static void merge(int[] result, int[] array1, int [] array2) {
+
+    }
+}
---
+++ src/test/java/org/example/SortTest.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import junit.framework.TestCase;
+
+import java.util.Arrays;
+
+public class SortTest extends TestCase {
+
+    public void testSortTwo() {
+        int[] testArray = new int[] {82, 19};
+        int[] expectedArray = new int[] {19, 82};
+        Sort.sortTwo(testArray);
+        assertTrue(Arrays.equals(testArray,expectedArray));
+    }
+
+    public void testMerge() {
+        int[] startingArray = new int[] {19,5,34,81,17,22,6};
+        int[] leftHalf = new int[] {5,19,34,81};
+        int[] rightHalf = new int[] {6,17,22};
+        int[] expectedResult = new int[] {5,6,17,19,22,34,81};
+        Sort.merge(startingArray,leftHalf,rightHalf);
+        assertTrue(Arrays.equals(startingArray, expectedResult));
+    }
+}
